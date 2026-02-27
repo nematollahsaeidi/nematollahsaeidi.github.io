901c1fb963c00eba522c60380edfe1405a5e9183
nematollahsaeidi <nemat.saeidi@gmail.com>
2026-02-27 23:52:01 +0000

    Deploying to gh-pages from @ nematollahsaeidi/nematollahsaeidi.github.io@6e0e36ad746f92afb63ac6d03f1effdcba46041d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/people/";
-          },
-        },{id: "dropdown-bookshelf",
+  },{id: "dropdown-bookshelf",
               title: "bookshelf",
               description: "",
               section: "Dropdown",
